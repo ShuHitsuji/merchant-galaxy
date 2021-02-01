c8aafe234145c0989c1ed4cb8826e61a0225ecfa
Axel <47676562+ShuHitsuji@users.noreply.github.com>
2021-01-31 21:40:53 -0300

    test update

--- src/test/java/ParagraphServiceTest.java
+++ src/test/java/ParagraphServiceTest.java
@@ -1,3 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+
+import java.util.Scanner;
+
+import static org.mockito.Mockito.when;
+
 public class ParagraphServiceTest {
 
+    private String romanError;
+    private String romanOk;
+    private String noInput;
+
+    @InjectMocks
+    ParagraphService paragraphService;
+
+    @Mock
+    Scanner scanner;
+
+    @Before
+    public void setUp() throws Exception {
+        romanError = "ABGJE";
+        romanOk = "MMMI";
+        noInput = "";
+    }
+
+    @Test
+    public void itShouldBeReturnError() throws RomanNumberException {
+        when(scanner.nextLine()).thenReturn(noInput);
+//Estuve buscando un rato como mockear el scanner y ni caso asi que me rendi
+        assert paragraphService.read().get(0).equals(ConstantValues.ErrorCode.NO_IDEA);
+    }
 }
--- src/test/java/RomanNumberServiceTest.java
+++ src/test/java/RomanNumberServiceTest.java
@@ -1,33 +1,29 @@
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
 
 public class RomanNumberServiceTest {
-    protected Character[] characterArray;
-    protected Character character;
-    protected String romanError;
-    protected String romanOk;
 
-    @Rule
-    public final ExpectedException exception = ExpectedException.none();
+    private String romanError;
+    private String romanOk;
+
+    @InjectMocks
+    RomanNumberService romanNumberService;
 
     @Before
     public void setUp() throws Exception {
-        character = 'P';
-        characterArray = new Character[]{'I','X','V','L'};
         romanError = "ABGJE";
         romanOk = "MMMI";
     }
 
     @Test
     public void itShouldBeReturnError() throws RomanNumberException {
-        assert RomanNumberService.romanToArabic(romanError).equals(ConstantValues.ErrorCode.INVALID_ROMAN_STRING);
+        assert romanNumberService.romanToArabic(romanError).equals(ConstantValues.ErrorCode.INVALID_ROMAN_STRING);
     }
 
     @Test
     public void itShouldBeOk() throws RomanNumberException {
         String romanResult = "3001";
-        assert RomanNumberService.romanToArabic(romanOk).equals(romanResult);
+        assert romanNumberService.romanToArabic(romanOk).equals(romanResult);
     }
 }
